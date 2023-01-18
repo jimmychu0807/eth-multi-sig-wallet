8030b87be2bd6089087a1dcc512b18176de91a42
Jimmy Chu <jimmychu0807@gmail.com>
2023-01-18 10:00:15 +0800

    updated

--- contracts/MetaMultiSigWallet.sol
+++ contracts/MetaMultiSigWallet.sol
@@ -2,11 +2,13 @@
 
 //  Off-chain signature gathering multisig that streams funds - @austingriffith
 //
-// started from 🏗 scaffold-eth - meta-multi-sig-wallet example https://github.com/austintgriffith/scaffold-eth/tree/meta-multi-sig
+// solhint-disable-next-line
+// started from 🏗 [scaffold-eth - meta-multi-sig-wallet example](https://github.com/austintgriffith/scaffold-eth/tree/meta-multi-sig)
 //    (off-chain signature based multi-sig)
 //  added a very simple streaming mechanism where `onlySelf` can open a withdraw-based stream
 
-pragma solidity >= 0.8.17 <0.9.0;
+// solhint-disable-next-line compiler-version
+pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
@@ -20,15 +22,36 @@ contract MetaMultiSigWallet {
   uint public nonce;
   uint public chainId;
 
-  constructor (uint _chainId, address[] memory _owners, uint _signaturesRequired) {
-    require(_owners.length >= _signaturesRequired, "Number of required signatures is greater than the number of owners.");
+  constructor (uint _chainId, address[] memory _owners, uint _signaturesRequired) public {
+    // solhint-disable-next-line reason-string
+    require(
+      _owners.length >= _signaturesRequired,
+      "Required signatures cannot be larger than the number of owners."
+    );
     chainId = _chainId;
     signaturesRequired = _signaturesRequired;
     nonce = 0;
     for (uint idx = 0; idx < _owners.length; idx++) {
       address owner = _owners[idx];
       require (owner != address(0), "Owner address cannot be 0x0.");
+      require (!isOwner[owner], "Duplicated owner.");
       isOwner[owner] = true;
     }
+  }
+
+  modifier onlySelf() {
+    require(msg.sender == address(this), "Cannot be called by others");
+    _;
+  }
+
+  function addSigner(address _newSigner) public onlySelf() {
+    require (owner != address(0), "Owner address cannot be 0x0.");
+    require(!isOwner[_newSigner], "Duplicated owner.");
+    isOwner[_newSigner] = true;
+    emit Owner(_newSigner, true);
+  }
+
+  receive() external payable {
+    emit Deposit(msg.sender, msg.value, address(this).balance);
   }
 }
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,27 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-solhint";
+import "hardhat-deploy";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  defaultNetwork: "hardhat",
+  namedAccounts: {
+    deployer: {
+      default: 0
+    },
+  },
+  networks: {
+    hardhat: {
+      live: false,
+      tags: ["local", "test"]
+    },
+    localhost: {
+      live: false,
+      url: "http://localhost:8545",
+      tags: ["local"]
+    }
+  }
 };
 
 export default config;
